99ed67b2a80f05686d4c53a49cef61f308153f5a
Mohd Ruknuddin <mohdruknuddin@Mohds-MacBook-Air.local>
2018-04-28 19:11:34 +0530

    custom table functionality introduced

---
+++ Arman/Model/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Arman
+//
+//  Created by Mohd Ruknuddin on 28/04/18.
+//  Copyright © 2018 Mohd Ruknuddin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
